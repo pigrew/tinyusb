f3c2b45d1b05e4fe08a47eed92dc63935977922b
Nathan Conrad <pigrew@gmail.com>
2019-09-09 13:31:58 -0400

    Fully remove F3

--- src/tusb_option.h
+++ src/tusb_option.h
@@ -52,9 +52,8 @@
 #define OPT_MCU_SAMD51        201 ///< MicroChip SAMD51
 
 #define OPT_MCU_STM32F4       300 ///< ST STM32F4
-#define OPT_MCU_STM32F3       301 ///< ST STM32F3
+#define OPT_MCU_STM32_FSDEV   301 ///< ST STM32F3
 #define OPT_MCU_STM32H7       302 ///< ST STM32H7
-#define OPT_MCU_STM32F0       303 ///< ST STM32F0
 
 /** @} */
 
