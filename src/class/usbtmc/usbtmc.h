2e13696faa024c5f004993d6d1197f52747ea1fd
Nathan Conrad <pigrew@gmail.com>
2019-09-10 16:59:45 -0400

    Many more defines for USBTMC.

--- src/class/usbtmc/usbtmc.h
+++ src/class/usbtmc/usbtmc.h
@@ -14,19 +14,19 @@
 
  If USB488DeviceCapabilites.D2 = 1 (SR1), then there must be a INT endpoint.
 */
-#define USBTMC_MSGID_DEV_DEP_MSG_OUT 1
-#define USBTMC_MSGID_REQUEST_DEV_DEP_MSG_IN 2
-#define USBTMC_MSGID_DEV_DEP_MSG_IN 2
-#define USBTMC_MSGID_VENDOR_SPECIFIC_OUT 126
-#define USBTMC_MSGID_REQUEST_VENDOR_SPECIFIC_IN 127
-#define USBTMC_MSGID_VENDOR_SPECIFIC_IN 127
-#define USBTMC_MSGID_USB488_TRIGGER 128
+typedef enum {
+  USBTMC_MSGID_DEV_DEP_MSG_OUT = 1u
+  #define USBTMC_MSGID_DEV_DEP_MSG_IN = 2u,
+  #define USBTMC_MSGID_VENDOR_SPECIFIC_MSG_OUT = 126u,
+  #define USBTMC_MSGID_VENDOR_SPECIFIC_IN = 127u,
+  #define USBTMC_MSGID_USB488_TRIGGER = 128u
+} usbtmc_msgid_enum;
 
 /// \brief Message header (For BULK OUT); 4 bytes
 typedef struct TU_ATTR_PACKED
 {
-  uint8_t MsgID              ; ///< Message ID
-  uint8_t bTag    			 ; ///< Transfer ID 1<=bTag<=255
+  uint8_t MsgID              ; ///< Message type ID (usbtmc_msgid_enum)
+  uint8_t bTag    		       ; ///< Transfer ID 1<=bTag<=255
   uint8_t bTagInverse        ; ///< Complement of the tag
   uint8_t Reserved           ; ///< Must be 0x00
 } usbtmc_msg_header_t;
@@ -45,16 +45,97 @@ typedef struct TU_ATTR_PACKED {
       uint8_t EOM  : 1         ; ///< EOM set on last byte
       uint8_t : 0;
   } bmTransferAttributes;
-} usbtmc_msg_request_dev_dep_msg_out;
+  uint8_t _reserved[3];
+} usbtmc_msg_request_dev_dep_out;
 
-#define USBTMC_APP_CLASS    0xFE
+// Next 8 bytes are message-specific
+typedef struct TU_ATTR_PACKED {
+  usbtmc_msg_header_t header ; ///< Header
+  uint32_t TransferSize      ; ///< Transfer size; LSB first
+  struct {
+      uint8_t : 0;
+      uint8_t TermCharEnabled  : 1 ; ///< "The Bulk-IN transfer must terminate on the specified TermChar."; CAPABILITIES must list TermChar
+  } bmTransferAttributes;
+  uint8_t TermChar;
+  uint8_t _reserved[2];
+} usbtmc_msg_request_dev_dep_in;
+
+
+
+typedef struct TU_ATTR_PACKED {
+  usbtmc_msg_header_t header ; ///< Header
+  uint32_t TransferSize      ; ///< Transfer size; LSB first
+  uint8_t _reserved[4];
+} usbtmc_msg_request_vendor_specific_out;
+
+
+typedef struct TU_ATTR_PACKED {
+  usbtmc_msg_header_t header ; ///< Header
+  uint32_t TransferSize      ; ///< Transfer size; LSB first
+  uint8_t _reserved[4];
+} usbtmc_msg_request_vendor_specific_in;
+
+// Control request type should use tusb_control_request_t
+
+/*
+typedef struct TU_ATTR_PACKED {
+  struct {
+    uint8_t Recipient  : 5         ; ///< EOM set on last byte
+    uint8_t Type       : 2         ; ///< EOM set on last byte
+    uint8_t DirectionToHost  : 1   ; ///< 0 is OUT, 1 is IN
+  } bmRequestType;
+  uint8_t bRequest                 ; ///< If bmRequestType.Type = Class, see usmtmc_request_type_enum
+  uint16_t wValue                  ;
+  uint16_t wIndex                  ;
+  uint16_t wLength                 ; // Number of bytes in data stage
+} usbtmc_class_specific_control_req;
+
+*/
+// bulk-in protocol errors
+enum {
+  USBTMC_BULK_IN_ERR_INCOMPLETE_HEADER = 1u,
+  USBTMC_BULK_IN_ERR_UNSUPPORTED = 2u,
+  USBTMC_BULK_IN_ERR_BAD_PARAMETER = 3u,
+  USBTMC_BULK_IN_ERR_DATA_TOO_SHORT = 4u,
+  USBTMC_BULK_IN_ERR_DATA_TOO_LONG = 5u,
+};
+// bult-in halt errors
+enum {
+  USBTMC_BULK_IN_ERR = 1u, ///< receives a USBTMC command message that expects a response while a
+                           /// Bulk-IN transfer is in progress
+};
+
+typedef enum {
+  USBTMC_bREQUEST_INITIATE_ABORT_BULK_OUT = 1u,
+  USBTMC_bREQUEST_CHECK_ABORT_BULK_OUT_STATUS = 2u,
+  USBTMC_bREQUEST_INITIATE_ABORT_BULK_IN = 3u,
+  USBTMC_bREQUEST_CHECK_ABORT_BULK_IN_STATUS = 4u,
+  USBTMC_bREQUEST_INITIATE_CLEAR = 5u,
+  USBTMC_bREQUEST_CHECK_CLEAR_STATUS = 6u,
+  USBTMC_bREQUEST_GET_CAPABILITIES = 7u,
+
+  USBTMC_bREQUEST_INDICATOR_PULSE = 64u, // Optional
+} usmtmc_request_type_enum;
+
+enum {
+  USBTMC_STATUS_SUCCESS = 0x01,
+  USBTMC_STATUS_PENDING = 0x02,
+  USBTMC_STATUS_FAILED = 0x80,
+  USBTMC_STATUS_TRANSFER_NOT_IN_PROGRESS = 0x81,
+  USBTMC_STATUS_SPLIT_NOT_IN_PROGRESS = 0x82,
+  USBTMC_STATUS_SPLIT_IN_PROGRESS  = 0x83
+};
+
+/************************************************************
+ * USBTMC Descriptor Templates
+ *************************************************************/
+
+#define USBTMC_APP_CLASS    TUSB_CLASS_APPLICATION_SPECIFIC
 #define USBTMC_APP_SUBCLASS 0x03
 
 #define USBTMC_PROTOCOL_STD    0x00
-#define SUBTMC_PROTOCOL_USB488 0x01
-// Other pro
+#define USBTMC_PROTOCOL_USB488 0x01
 
-// CDC Descriptor Template
 //   Interface number, number of endpoints, EP string index, USB_TMC_PROTOCOL*, bulk-out endpoint ID,
 //   bulk-in endpoint ID
 #define USBTMC_IF_DESCRIPTOR(_itfnum, _bNumEndpoints, _stridx, _itfProtocol) \
